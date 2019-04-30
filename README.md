cc1842a7a667245f8bb5ad56d71e62b35bb61c90
Erhan Yakut <erhan.yakut@icsdefense.net>
2019-05-01 00:27:21 +0300

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# php-nats-client
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This example show how to use PHP NATS Client
+ *
+ * @package    PHP NATS
+ * @author     Erhan Yakut <yakuter@gmail.com>
+ * @copyright  2019 Yakuter
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    1.0.0
+ * @link       https://github.com/yakuter/php-nats-client
+ */
 
 require_once 'vendor/autoload.php';
 
@@ -12,22 +22,22 @@ $connectionOptions->setHost('localhost')->setPort(4222);
 // Yeni bağlantı nesnesi oluşturma ve bağlanma
 $client = new \Nats\Connection($connectionOptions);
 $client->connect();
-printf('Bağlanılan NATS Server id: %s <br>', $client->connectedServerId());
+printf('Bağlanılan NATS Server id: <strong>%s</strong> <br>', $client->connectedServerId());
 
 // Basit Subscribe olma
-$client->subscribe(
-    'foo',
-    function ($message) {
-        printf(PHP_EOL."Data: %s\r\n", $message->getBody());
-    }
-);
+$callback = function($payload)
+{
+    printf("Data: <strong>%s</strong>", $payload);
+};
+$sid = $client->subscribe("foo", $callback);
 
 // Basit Publish yapma
-//$client->publish('foo', 'Marty McFly');
+$client->publish('foo', 'Marty McFly');
 
 // 1 mesaj gelene kadar bekletme
 $client->wait(1);
+$client->unsubscribe($sid);
 
 // Bağlantıyı sonlandır
-$client->close();
+//$client->close();
 ?>
